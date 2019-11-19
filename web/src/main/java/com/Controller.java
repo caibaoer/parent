cc37e56284f6c0fd2edd599c96adac3358e03085
huangwencai <18580445131@139.com>
2019-11-19 09:08:45 +0800

    m

---
+++ web/src/main/java/com/Controller.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * @desc：
+ * @title：Controller
+ * @author: huangwencai
+ * @date: 2019-11-18 9:40
+ * @version: v4.40.0
+ */
+
+/**
+ * 好像按照现在这种做法，不是springboot模式，这里想对外提供访问很难哦...
+ */
+public class Controller {
+
+}
